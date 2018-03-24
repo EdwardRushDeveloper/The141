cab0d7bbc3b7459d8c602dffa1a54ee690595eb4
EdwardRush <edward.rush.developer@live.com>
2018-03-24 15:40:48 -0400

    Updated Work for Class Web141

---
+++ The141/wwwroot/pages/scripts/page1.js
@@ -0,0 +1,27 @@
+﻿
+
+Page = {}
+Page.Class = function()
+{
+    var _this = this;
+
+     $s = function(id)
+     {
+        return document.getElementById(id);
+     }
+
+    _this.SearchButton = $s('PageSearchButton');
+    _this.SearchText   = $s('PageSearchText');
+
+    _this.SearchButton.Search = _this.SearchText;
+
+    _this.SearchButton.onclick = function()
+    {
+        var searchThis = "https://www.google.com/search?q=" + this.Search.value;
+
+        window.open(searchThis);
+
+    }
+}
+
+
